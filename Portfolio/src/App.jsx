7ad92aba03ce3b2410aff85cc9906ca83ac3abda
nihal <nihalmurali047@gmail.com>
2025-06-27 12:11:37 +0530

    Moved Navbar outside the Route tag so it gets displayed all over the webpage

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 const App = () => {
   return (
     <BrowserRouter>
-      <Route path="/nav" element={<Navbar/>}/>
+      <Navbar/>
       <Routes>
       </Routes>
     </BrowserRouter>
