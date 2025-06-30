5d5651afd0ae03efb0dfb04ce27abe94d4443c45
nihal <nihalmurali047@gmail.com>
2025-06-30 13:50:38 +0530

    Almost finished the navbar, Created a few different logos and shortlisted one which had been added to the Navbar.

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar/>
       <Routes>
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   )
--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/navbar.css'
+import { Link } from 'react-router-dom'
+import myLogo from '../assets/my-logo4.png'
 
 const Navbar = () => {
+  
+  const [openMenu , setOpenMenu] = useState('false');
+  const [navUnderline, setNavUnderline] = useState('false');
+
   return (
     <>
-        <div className="navbar-container">
-            Navbar
+        <nav className="navbar-container arvo-regular">
+            <div className='logo'>
+              <img src={myLogo} alt="myLogo" srcset=""/>
+            </div>
+            <div className="nav-links">
+              <a href="/" className={`${navUnderline? "active": ""}`} onClick={()=>setNavUnderline((active)=>!active)}>Home</a>
+              <a href="">Skills</a>
+              <a href="">Projects</a>
+              <a href="">Experience</a>
+              <a href="">Education and Certifications</a>
+            </div>
+            <div className="menuButton" id='menu' onClick={()=>{setOpenMenu((prev)=> !prev)}}>☰</div>
+        </nav>
+        <div className={`dropdownMenu ${openMenu? "flex":"none"} arvo-regular`} id='dropdown-menu'>
+          <a href="">Home</a>
+          <a href="">Skills</a>
+          <a href="">Projects</a>
+          <a href="">Experience</a>
+          <a href="">Education and Certifications</a>
         </div>
     </>
   )
